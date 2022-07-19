9c8fd49fc8c957f3397b4c9f61daf387c2c2387c
wjuniori <wjuniori.si@gmail.com>
2022-07-19 16:28:28 -0300

    challenge finished

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,34 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const newTaskTitleTrim = newTaskTitle.trim();
+
+    if (newTaskTitleTrim.length > 0) {
+      const newTask: Task = {
+        id: Math.random(),
+        title: newTaskTitleTrim,
+        isComplete: false,
+      };
+
+      setTasks([...tasks, newTask]); // OR setTasks(tasks => [...tasks, newTask]);
+      setNewTaskTitle('');
+    }
   }
 
+  // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => 
+      task.id === id ? {...task, isComplete: !task.isComplete} : task
+    );
+    setTasks(newTasks);
   }
 
+  // Remova uma task da listagem pelo ID
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks);
   }
 
   return (
